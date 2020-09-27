3ce8364a9586114ec91878652afea3e5ebb8471e
Jean Paul | Origem <jeanpaul2022@gmail.com>
2020-09-27 00:01:51 -0300

    Removendo header

--- App.js
+++ App.js
@@ -2,15 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaView, Text, View } from "react-native";
 import List from "./components/list";
-import ContextProvider from "./context/headerContext";
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#ECECEC" }}>
       <StatusBar hidden />
-      <ContextProvider>
-        <List />
-      </ContextProvider>
+      <List />
     </SafeAreaView>
   );
 }
--- components/list.js
+++ components/list.js
@@ -1,13 +1,8 @@
 import React, { useEffect, useState, useRef } from "react";
 import { View, Text, FlatList, Animated, Dimensions } from "react-native";
-import Header from "./header";
-import { useHeader } from "../context/headerContext";
-const HeaderAnimation = Animated.createAnimatedComponent(View);
 
 const List = () => {
   const [data, setData] = useState([]);
-  const scrollY = useRef(new Animated.Value(0)).current;
-  const { state, setState } = useHeader();
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -32,43 +27,13 @@ const List = () => {
     );
   };
 
-  // useEffect(() => {
-  //   scrollY.addListener((e) => setState(Math.floor(e.value)));
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log("e:", state);
-  // }, [state]);
-
   return (
-    <>
-      <Animated.View
-        style={{
-          height: 60,
-          zIndex: 1,
-          transform: [
-            {
-              translateX: scrollY.interpolate({
-                inputRange: [0, 10],
-                outputRange: [0, 20],
-              }),
-            },
-          ],
-        }}
-      >
-        <Header />
-      </Animated.View>
-
-      <Animated.FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item) => String(item.id)}
-        onScroll={Animated.event(
-          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
-          { useNativeDriver: true }
-        )}
-      />
-    </>
+    <Animated.FlatList
+      data={data}
+      renderItem={renderItem}
+      ListEmptyComponent={<Text>Carregando...</Text>}
+      keyExtractor={(item) => String(item.id)}
+    />
   );
 };
 
